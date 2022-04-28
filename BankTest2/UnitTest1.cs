40da9d893b3b0ab9368c4232e530883e1061634a
Davis Johnson <davbjohn@ut.utm.edu>
2022-04-27 16:37:28 -1000

    a

--- BankTest2/UnitTest1.cs
+++ BankTest2/UnitTest1.cs
@@ -58,7 +58,6 @@ namespace BankTest2
 
         //unit test method  
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
         {
             // arrange  
@@ -67,9 +66,20 @@ namespace BankTest2
             BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
 
             // act  
-            account.Debit(debitAmount);
 
             // assert is handled by ExpectedException  
+
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert  
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
         }
     }
 }
